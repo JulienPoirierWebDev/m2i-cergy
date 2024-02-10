8688d42eeab89d894924b833df356d2ef7257298
bootcamp <formation.rubentrottein@gmail.com>
2024-02-11 00:13:23 +0100

    modifications de vendredi

--- ruben/tp/main.js
+++ ruben/tp/main.js
@@ -1,1 +1,18 @@
 import './style.css'
+//import './theme.css'
+
+document.querySelector(".closeMenu").addEventListener("click", ()=>{
+    document.querySelector("details").open = closed;
+})
+
+for(let link of document.querySelectorAll("nav a")){
+    link.addEventListener("click",(e)=>{
+        e.preventDefault();
+        alert(document.querySelector("footer").textContent);
+    })
+}
+document.querySelector("button").addEventListener("click",(e)=>{
+    e.preventDefault();
+    alert(document.querySelector("footer").textContent);
+})
+
