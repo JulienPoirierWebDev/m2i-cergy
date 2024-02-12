c32f3f35080491cf0e94e6ffc6a816eb6d76d08a
rubentrottein <38758157+rubentrottein@users.noreply.github.com>
2024-02-12 11:57:56 +0100

    Aveu d'échec sur l'installation de Tailwind mais on garde le sourire

--- ruben/tailwind/tailwind.config.js
+++ ruben/tailwind/tailwind.config.js
@@ -1,7 +1,9 @@
-module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
