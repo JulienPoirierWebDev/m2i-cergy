821ec380263e2ca0ed1b175202ef2cb9d35a7017
androd22 <andy_14@hotmail.fr>
2024-02-12 17:06:35 +0100

    avancé sur le site de vacances tailwind

---
+++ TW_FatnaB/exo1/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "li-nav-color": "#957554",
+      },
+    },
+  },
+  plugins: [],
+};
